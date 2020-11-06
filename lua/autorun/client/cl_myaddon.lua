5f5c5d1ed5a2eba6f51683b8312c58303e98759c
KonnorGreen <jmldgreen6@gmail.com>
2020-11-06 11:04:47 -0600

    Added health bar hook demo

--- lua/autorun/client/cl_myaddon.lua
+++ lua/autorun/client/cl_myaddon.lua
@@ -1,5 +1,15 @@
 include("autorun/sh_myaddon.lua")
 
-local myName = "Konnor"
-print(myName)
-print(MYADDON_NAME)
+hook.Add("HUDPaint", "MyAddonHUD", function()
+    local ply = LocalPlayer() --setting the player as a var
+    local hp = ply:Health() --getting players health
+    local maxhp = ply:GetMaxHealth()
+    local scrw,scrh = ScrW(), ScrH()
+    local boxW = scrw * .5
+    local boxH = scrh * .04
+    surface.SetDrawColor(0,0,0,200)
+    surface.DrawRect(scrw/2-boxW/2, scrh - boxH *1.1, boxW, boxH)
+    surface.SetDrawColor(255,0,0,200)
+    surface.DrawRect(scrw/2-boxW/2, scrh - boxH *1.1, boxW * (hp / maxhp), boxH)
+    
+end)
--- lua/autorun/server/sv_myaddon.lua
+++ lua/autorun/server/sv_myaddon.lua
@@ -1,5 +1,7 @@
 include("autorun/sh_myaddon.lua")
-local function addition()
-    print("You called addition!")
-end
-addition()
+hook.Add("PlayerSay", "MyAddonChatCommands", function(sender,text,teamChat)
+    if text == "!crowbar" then
+        sender:Give("weapon_crowbar")
+        sender:ChatPrint("You received a crowbar!")
+    end
+end)
